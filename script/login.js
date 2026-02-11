103d08b619d86da279500955f659bae958d9d727
Tisha Dey <tishadey2025@outlook.com>
2026-02-11 17:34:45 +0600

    :green_heart: main page added

--- script/login.js
+++ script/login.js
@@ -1,3 +1,15 @@
-document.getElementById("login-btn").addEventListener("click", function(){
-console.log("hello jaan");
+document.getElementById("login-btn").addEventListener("click", function(event){
+event.preventDefault();
+const accountNumber=document.getElementById("account-number").value;
+const pin=document.getElementById("pin").value;
+const convertedPin=parseInt(pin) //pin ke numbere convert korci // 
+if(accountNumber.length===11){
+    if(convertedPin===1234){ //1234 ke "" diye string kora jeto //
+   window.location.href="./main.html"
+    }else{
+        console.log("pin tik nai");
+    }
+}else{
+    console.log("need valid account number");
+}
 })
